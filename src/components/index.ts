6f63f8f464f1b11262739151af75d23fc1ab9cd7
leobaltazor <leo.baltazor@gmail.com>
2019-11-08 16:44:20 +0200

    chore(components): index added for component folder

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as InputField } from "./inputs/InputField.vue";
+export { default as TaskComponent } from "./TaskComponent.vue";
+export { default as TaskListComponent } from "./TaskListComponent.vue";
